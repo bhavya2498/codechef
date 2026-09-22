cdce1794587624c08c148c662ec567695e34fd6b
bhavya2498 <bhavyashrree14@gmail.com>
2026-09-22 21:53:31 +0530

    Solved: SQLPBP02 (codechef)

---
+++ codechef/Practice/SQLPBP02.sql
@@ -0,0 +1,10 @@
+// Problem: SQLPBP02
+// Platform: codechef
+// Language: SQL​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP02
+// Solved on: 2026-09-22T16:23:31.249Z
+
+SELECT COUNT(*) AS fiction_count
+FROM Books
+WHERE genre = 'Fiction';
