385a17f78c9dec2855f296bfbeffdea17a0cfe45
bhavya2498 <bhavyashrree14@gmail.com>
2026-09-22 21:53:29 +0530

    Solved: SQLPBP13 (codechef)

---
+++ codechef/Practice/SQLPBP13.sql
@@ -0,0 +1,9 @@
+// Problem: SQLPBP13
+// Platform: codechef
+// Language: SQL​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP13
+// Solved on: 2026-09-22T16:23:29.421Z
+
+SELECT DISTINCT company_name
+FROM Works;
