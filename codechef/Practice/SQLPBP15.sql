92f9932fc4731f738bf4cf208b356ca16fdbe855
bhavya2498 <bhavyashrree14@gmail.com>
2026-09-22 21:53:28 +0530

    Solved: SQLPBP15 (codechef)

---
+++ codechef/Practice/SQLPBP15.sql
@@ -0,0 +1,10 @@
+// Problem: SQLPBP15
+// Platform: codechef
+// Language: SQL​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP15
+// Solved on: 2026-09-22T16:23:27.773Z
+
+SELECT department_name, location
+FROM departments
+WHERE location LIKE 'S%';
