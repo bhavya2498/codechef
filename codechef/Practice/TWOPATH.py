99bf02915bd4a4ace58668f88c3625f863cb77c5
bhavya2498 <bhavyashrree14@gmail.com>
2026-09-23 21:48:16 +0530

    Solved: TWOPATH (codechef)

--- codechef/Practice/TWOPATH.py
+++ codechef/Practice/TWOPATH.py
@@ -3,6 +3,6 @@
 // Language: Python3​
 // Verdict: Accepted
 // URL: https://www.codechef.com/START257D/problems/TWOPATH
-// Solved on: 2026-09-23T16:15:57.885Z
+// Solved on: 2026-09-23T16:18:15.324Z
 
 # cook your dish here
