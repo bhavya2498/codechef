fd8a74303f7c8ee08c22a1eb5b4d6730930a4401
bhavya2498 <bhavyashrree14@gmail.com>
2026-09-23 21:28:29 +0530

    Solved: FALLPR (codechef)

---
+++ codechef/Practice/FALLPR.py
@@ -0,0 +1,8 @@
+// Problem: FALLPR
+// Platform: codechef
+// Language: Python3​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START257D/problems/FALLPR
+// Solved on: 2026-09-23T15:58:28.381Z
+
+# cook your dish here
