f9781f5ca8bb4be2b43dd22bbe88002ec3766003
bhavya2498 <bhavyashrree14@gmail.com>
2026-09-23 20:00:21 +0530

    Solved: LPYAS02 (codechef)

---
+++ codechef/Practice/LPYAS02.c
@@ -0,0 +1,8 @@
+// Problem: LPYAS02
+// Platform: codechef
+// Language: Coding, Chef!
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/python/LPPYAS01/problems/LPYAS02
+// Solved on: 2026-09-23T14:30:20.387Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
