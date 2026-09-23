a8c795331c236cbc219b3a7faa963cac02f422a2
bhavya2498 <bhavyashrree14@gmail.com>
2026-09-23 20:04:58 +0530

    Solved: LPYAS10B (codechef)

---
+++ codechef/Practice/LPYAS10B.py
@@ -0,0 +1,13 @@
+// Problem: LPYAS10B
+// Platform: codechef
+// Language: Python3​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/python/LPPYAS01/problems/LPYAS10B
+// Solved on: 2026-09-23T14:34:57.778Z
+
+# cook your dish 
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
