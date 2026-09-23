b422c6494262e6c6d4a4c1b2a7c25d812238936f
bhavya2498 <bhavyashrree14@gmail.com>
2026-09-23 20:03:06 +0530

    Solved: LPYAS09 (codechef)

---
+++ codechef/Practice/LPYAS09.py
@@ -0,0 +1,9 @@
+// Problem: LPYAS09
+// Platform: codechef
+// Language: Python3​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/python/LPPYAS01/problems/LPYAS09
+// Solved on: 2026-09-23T14:33:04.963Z
+
+# cook your dish here
+print(12 * 12 - 9 * 4)
