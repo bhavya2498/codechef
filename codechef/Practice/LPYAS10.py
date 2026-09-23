b013655ac661161fd3b1989c89555c0e5d5ed86b
bhavya2498 <bhavyashrree14@gmail.com>
2026-09-23 20:04:07 +0530

    Solved: LPYAS10 (codechef)

---
+++ codechef/Practice/LPYAS10.py
@@ -0,0 +1,9 @@
+// Problem: LPYAS10
+// Platform: codechef
+// Language: Python3​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/python/LPPYAS01/problems/LPYAS10
+// Solved on: 2026-09-23T14:34:06.184Z
+
+# cook your dish here
+print("Learn Coding on CodeChef")
