7d0c5d88025c1b55b9b1fc61066ac8d2ba45a85a
bhavya2498 <bhavyashrree14@gmail.com>
2026-09-23 21:31:20 +0530

    Solved: MKSMT (codechef)

---
+++ codechef/Practice/MKSMT.py
@@ -0,0 +1,8 @@
+// Problem: MKSMT
+// Platform: codechef
+// Language: Python3​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START257D/problems/MKSMT
+// Solved on: 2026-09-23T16:01:20.161Z
+
+# cook your dish here
