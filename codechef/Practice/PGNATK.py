4fde754bb179510feabec6fab06b6f8f9a127de8
bhavya2498 <bhavyashrree14@gmail.com>
2026-09-23 21:26:20 +0530

    Solved: PGNATK (codechef)

---
+++ codechef/Practice/PGNATK.py
@@ -0,0 +1,8 @@
+// Problem: PGNATK
+// Platform: codechef
+// Language: Python3​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START257D/problems/PGNATK
+// Solved on: 2026-09-23T15:56:19.754Z
+
+# cook your dish here
