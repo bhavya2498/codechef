976d9caffe9b787e7f0ab6019587b906d9a9ee44
bhavya2498 <bhavyashrree14@gmail.com>
2026-09-23 21:34:43 +0530

    Solved: FUMI (codechef)

---
+++ codechef/Practice/FUMI.py
@@ -0,0 +1,8 @@
+// Problem: FUMI
+// Platform: codechef
+// Language: Python3​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START257D/problems/FUMI
+// Solved on: 2026-09-23T16:04:43.015Z
+
+# cook your dish here
