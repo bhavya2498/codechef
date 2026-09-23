f81e51e367782bfedb2d100c13e08fb9d3aa8eb2
bhavya2498 <bhavyashrree14@gmail.com>
2026-09-23 21:28:22 +0530

    Solved: LITUP (codechef)

--- codechef/Practice/LITUP.py
+++ codechef/Practice/LITUP.py
@@ -3,6 +3,6 @@
 // Language: Python3​
 // Verdict: Accepted
 // URL: https://www.codechef.com/START257D/problems/LITUP
-// Solved on: 2026-09-23T15:57:06.383Z
+// Solved on: 2026-09-23T15:58:21.174Z
 
-# cook your dish here
+// source not captured automatically - open the solution page and copy it manually
