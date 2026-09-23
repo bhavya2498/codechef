64497e9263a981dc66e4529878f48130519ec7e1
bhavya2498 <bhavyashrree14@gmail.com>
2026-09-23 21:39:03 +0530

    Solved: FUMI (codechef)

--- codechef/Practice/FUMI.py
+++ codechef/Practice/FUMI.py
@@ -3,19 +3,6 @@
 // Language: Python3​
 // Verdict: Accepted
 // URL: https://www.codechef.com/START257D/problems/FUMI
-// Solved on: 2026-09-23T16:06:34.741Z
+// Solved on: 2026-09-23T16:09:02.668Z
 
-# cook your dish here
-T = int(input())
-
-for _ in range(T):
-    N, A, B, C = map(int, input().split())
-
-    ans = 10**30
-
-    for d in range(1, N + 1):
-        z = (N + d - 1) // d
-        cost = (A + B) * d + C * z
-        ans = min(ans, cost)
-
-    print(ans)
+// source not captured automatically - open the solution page and copy it manually
