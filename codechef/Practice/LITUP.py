c7b5ddcf682ac82448782baf4f2d89fb27fdf8e4
bhavya2498 <bhavyashrree14@gmail.com>
2026-09-23 21:27:07 +0530

    Solved: LITUP (codechef)

---
+++ codechef/Practice/LITUP.py
@@ -0,0 +1,8 @@
+// Problem: LITUP
+// Platform: codechef
+// Language: Python3​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START257D/problems/LITUP
+// Solved on: 2026-09-23T15:57:06.383Z
+
+# cook your dish here
