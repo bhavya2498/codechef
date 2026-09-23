cb9d4e98307c1cd2328a069c5ba83254682d5ddb
bhavya2498 <bhavyashrree14@gmail.com>
2026-09-23 21:25:23 +0530

    Solved: ECHOECHO (codechef)

---
+++ codechef/Practice/ECHOECHO.java
@@ -0,0 +1,19 @@
+// Problem: ECHOECHO
+// Platform: codechef
+// Language: Java​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START257D/problems/ECHOECHO
+// Solved on: 2026-09-23T15:55:22.716Z
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+
+	}
+}
