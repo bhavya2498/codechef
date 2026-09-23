31e4ae6b13c6fe377aa90adac48b64150a07c9e2
bhavya2498 <bhavyashrree14@gmail.com>
2026-09-23 21:30:08 +0530

    Solved: FALLPR (codechef)

--- codechef/Practice/FALLPR.py
+++ codechef/Practice/FALLPR.py
@@ -3,6 +3,6 @@
 // Language: Python3​
 // Verdict: Accepted
 // URL: https://www.codechef.com/START257D/problems/FALLPR
-// Solved on: 2026-09-23T15:58:28.381Z
+// Solved on: 2026-09-23T16:00:07.165Z
 
-# cook your dish here
+// source not captured automatically - open the solution page and copy it manually
