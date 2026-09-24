98c49f7320a7fcc5033927c0bf1c2665be40e676
bhavya2498 <bhavyashrree14@gmail.com>
2026-09-24 22:34:24 +0530

    Solved: LPYAS11 (codechef)

---
+++ codechef/Practice/LPYAS11.c
@@ -0,0 +1,8 @@
+// Problem: LPYAS11
+// Platform: codechef
+// Language: print("Code", "Chef")
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/python/LPPYAS02/problems/LPYAS11
+// Solved on: 2026-09-24T17:04:23.390Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
